ac4b6dc7c292d35560131e73c1b2e563019b4894
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-05-05 20:20:38 +0600

    error handle jwt

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ const port = process.env.PORT || 5000
 
 //3 
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 //4 middleware 
 app.use(cors())
 app.use(express.json())
+
+//JWT 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 //5 mongo connect
 
@@ -24,6 +42,15 @@ async function run() {
         await client.connect();
         const cameraCollection = client.db("warehouse").collection("camera");
 
+        // 16 jwt 
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
+
         //8 get all camera
         app.get('/product', async (req, res) => {
             //15 pagination 
@@ -31,25 +58,30 @@ async function run() {
             const pageProduct = parseInt(req.query.pageProduct)
             const query = {}
             const cursor = cameraCollection.find(query)
-            let products ;
-            if(page || pageProduct){
-                products = await cursor.skip(page*pageProduct).limit(pageProduct).toArray()
+            let products;
+            if (page || pageProduct) {
+                products = await cursor.skip(page * pageProduct).limit(pageProduct).toArray()
             }
-            else{
+            else {
                 products = await cursor.toArray()
             }
             res.send(products)
-            
+
         })
 
         //13 my items 
-        app.get('/my-items' , async(req,res) => {
-            const email = req.query.email ;
-            const query = {email : email} 
-            console.log(query);
-            const cursor = cameraCollection.find(query)
-            const myItems = await cursor.toArray()
-            res.send(myItems)
+        app.get('/my-items', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email.value;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = cameraCollection.find(query)
+                const myItems = await cursor.toArray()
+                res.send(myItems)
+            }
+            else{
+                res.status(403).send({message: 'Forbidden Access'})
+            }
         })
 
         //9 get one camera
@@ -61,19 +93,16 @@ async function run() {
         })
 
         //12 Stoke item api 
-        app.post('/product' , async(req,res) => {
-            const product = req.body 
+        app.post('/product', async (req, res) => {
+            const product = req.body
             const result = await cameraCollection.insertOne(product)
             res.send(result)
         })
 
-        // {name,description,price,quantity,img,supplier,afPoints , brand,brandId,modes,movieType,sold,ratings}
-
-
         //11 delete product 
-        app.delete('/product/:id' , async(req,res) => {
-            const id = req.params.id 
-            const query = {_id:ObjectId(id)}
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
             const camera = await cameraCollection.deleteOne(query)
             res.send(camera)
         })
@@ -92,7 +121,7 @@ async function run() {
             };
             const result = await cameraCollection.updateOne(filter, updateDoc, options);
             res.send(result)
-            
+
         })
 
         //14 pagination    
@@ -100,7 +129,6 @@ async function run() {
             const count = await cameraCollection.estimatedDocumentCount()
             res.send({ count })
         })
-
 
     }
 
